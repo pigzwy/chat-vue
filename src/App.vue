abdbcfdf4125421e7e8028ea22e35beec6445825
pig <183850523@qq.com>
2026-04-25 00:25:53 +0800

    修改标题

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 import { computed } from 'vue'
 import { useHead } from '@unhead/vue'
 import { useColorMode } from '@vueuse/core'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
 const colorMode = useColorMode()
 const themeColor = computed(() => colorMode.value === 'dark' ? '#09090b' : '#fafafa')
+const title = computed(() => route.path.startsWith('/images') ? '画图 - PIG Coder' : '对话 - PIG Coder')
 
 useHead({
+  title,
   meta: [
     { name: 'theme-color', content: themeColor }
   ]
